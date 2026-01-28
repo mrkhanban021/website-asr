2942dd51840b09dd471dec845ef4210dab4bb3d6
ahmadreza <ahmdrzakhanban@gmail.com>
2026-01-28 17:24:56 +0330

    bootsrap config offline

--- core/settings.py
+++ core/settings.py
@@ -1,5 +1,7 @@
 
 from pathlib import Path
+import os
+import environ
 
 
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,6 +23,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'website.apps.WebsiteConfig',
 ]
 
 MIDDLEWARE = [
@@ -38,7 +41,7 @@ ROOT_URLCONF = 'core.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / "templates"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -51,6 +54,7 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'core.wsgi.application'
+ASGI_APPLICATION = 'core.asgi.application'
 
 
 
@@ -95,3 +99,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR / 'static')
+
+STATICFILES_DIRS  = [
+    BASE_DIR / 'assets'
+]
+
+MEDIA_URL = 'media/'
+MEDIA_ROOT = os.path.join(BASE_DIR / "media")
